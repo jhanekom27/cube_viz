e787f1c37e972737be402487d25719c0a837c308
Jurgen Hanekom <hanekom.jurgen@bcg.com>
2020-10-17 12:42:57 +0930

    create app in its own file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
+import pandas as pd
 import os
 from dotenv import load_dotenv
 
-import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
@@ -10,52 +10,53 @@ from dash.dependencies import Input, Output, State
 from cubeviz.charts import plot_solve_time_series
 from cubeviz.io import parse_upload_content
 from cubeviz.etl import process_timiks_data
+from cubeviz.server import app
+import cubeviz.callback
 
 load_dotenv()
 
 window_sizes = [25, 50, 100]
 
-app = dash.Dash(__name__)
-server = app.server
 
-app.layout = html.Div(
-    [
-        dcc.Upload(
-            id="upload-data",
-            children=html.Div(["Drag and Drop or ", html.A("Select Files")]),
-            style={
-                "width": "100%",
-                "height": "60px",
-                "lineHeight": "60px",
-                "borderWidth": "1px",
-                "borderStyle": "dashed",
-                "borderRadius": "5px",
-                "textAlign": "center",
-                "margin": "10px",
-            },
-            multiple=False,
-        ),
-        html.Div(id="div-solve-time-series"),
-    ],
-)
+def get_app(app):
+    app.layout = html.Div(
+        [
+            dcc.Upload(
+                id="upload-data",
+                children=html.Div(["Drag and Drop or ", html.A("Select Files")]),
+                style={
+                    "width": "100%",
+                    "height": "60px",
+                    "lineHeight": "60px",
+                    "borderWidth": "1px",
+                    "borderStyle": "dashed",
+                    "borderRadius": "5px",
+                    "textAlign": "center",
+                    "margin": "10px",
+                },
+                multiple=False,
+            ),
+            html.Div(id="div-solve-time-series"),
+            dcc.Dropdown(
+                id="dropdown",
+                options=[{"label": i, "value": i} for i in ["LA", "NYC", "MTL"]],
+                value="LA",
+            ),
+            html.Div(id="display-value"),
+        ],
+    )
+    return app
 
 
-@app.callback(
-    Output("div-solve-time-series", "children"),
-    [Input("upload-data", "contents")],
-    [State("upload-data", "filename"), State("upload-data", "last_modified")],
-)
-def update_output(content, name, date):
-    if content is not None:
-        df_raw = parse_upload_content(content, name, date)
-        # TODO: add another layer to not process timiks but process some standard form
-        df_clean = process_timiks_data(df_raw, window_sizes)
-        df_mins = df_clean[df_clean["is_diff"] == True]
-        fig = plot_solve_time_series(df_clean, df_mins, window_sizes)
-        return dcc.Graph("main-graph", figure=fig, className="big-plot")
+def plot_thing(df_clean):
+    df_mins = df_clean[df_clean["is_diff"] == True]
+    fig = plot_solve_time_series(df_clean, df_mins, window_sizes)
+    return fig
 
 
 if __name__ == "__main__":
+    app = get_app(app)
+
     environment = os.getenv("ENVIRONMENT")
     if environment == "local":
         app.run_server(host="127.0.0.1", port=8000, debug=True)
---
+++ cubeviz/callback.py
@@ -0,0 +1,31 @@
+from cubeviz.server import app
+from dash.dependencies import Input, Output, State
+import pandas as pd
+from cubeviz.io import parse_upload_content
+from cubeviz.etl import process_timiks_data
+from cubeviz.charts import plot_solve_time_series
+import dash_core_components as dcc
+
+window_sizes = [25, 50, 100]
+
+
+@app.callback(
+    Output("div-solve-time-series", "children"),
+    [Input("upload-data", "contents")],
+    [State("upload-data", "filename"), State("upload-data", "last_modified")],
+)
+def update_output(content, name, date):
+    if content is not None:
+        df_raw = parse_upload_content(content, name, date)
+        # TODO: add another layer to not process timiks but process some standard form
+        df_clean = process_timiks_data(df_raw, window_sizes)
+        df_mins = df_clean[df_clean["is_diff"] == True]
+        fig = plot_solve_time_series(df_clean, df_mins, window_sizes)
+        return dcc.Graph("main-graph", figure=fig, className="big-plot")
+    else:
+        df_raw = pd.read_csv("data/sample.csv")
+        df_clean = process_timiks_data(df_raw, window_sizes)
+        df_mins = df_clean[df_clean["is_diff"] == True]
+        fig = plot_solve_time_series(df_clean, df_mins, window_sizes)
+        return dcc.Graph("main-graph", figure=fig, className="big-plot")
+
