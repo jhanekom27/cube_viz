a92b48349b3aa0d3d217c3cb93486e8c6b0c19e2
Jurgen Hanekom <hanekom.jurgen@bcg.com>
2020-10-17 13:04:29 +0930

    added config loader

---
+++ cubeviz/config.py
@@ -0,0 +1,19 @@
+from yamlinclude import YamlIncludeConstructor
+import yaml
+from dacite import from_dict
+from typing import List
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    window_sizes: List[int]
+
+
+def load_config():
+    YamlIncludeConstructor.add_to_loader_class(loader_class=yaml.FullLoader)
+
+    with open("config.yaml", "r") as file:
+        yaml_as_dictionary = yaml.load(file, Loader=yaml.FullLoader)
+
+    return from_dict(data_class=Config, data=yaml_as_dictionary)
