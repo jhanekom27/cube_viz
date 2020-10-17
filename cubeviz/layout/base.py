8f74cd8821d98e80dcb05f52557aca11ae955e15
Jurgen Hanekom <hanekom.jurgen@bcg.com>
2020-10-17 20:30:13 +0930

    tabs to own functions

--- cubeviz/layout/base.py
+++ cubeviz/layout/base.py
@@ -1,34 +1,8 @@
 import dash_html_components as html
 import dash_core_components as dcc
 
-from cubeviz.helpers import load_text
-
-
-def upload_window():
-    return dcc.Upload(
-        id="upload-data",
-        children=html.Div(["Drag and Drop or ", html.A("Select Files")]),
-        style={
-            # "width": "100%",
-            "height": "60px",
-            "lineHeight": "60px",
-            "borderWidth": "1px",
-            "borderStyle": "dashed",
-            "borderRadius": "5px",
-            "textAlign": "center",
-            "margin": "10px",
-            "color": "rgb(90, 90, 90)",
-        },
-        multiple=False,
-    )
-
-
-def info_markdown():
-    return dcc.Markdown(load_text("cubeviz/markdown/info.md"))
-
-
-def info_tab_layout():
-    return [upload_window(), info_markdown()]
+from cubeviz.layout.tab_info import info_tab_layout
+from cubeviz.layout.tab_solve_times import solve_times_tab_layout
 
 
 def build_app(app):
@@ -37,7 +11,7 @@ def build_app(app):
             dcc.Tabs(
                 [
                     dcc.Tab(label="Info", children=info_tab_layout()),
-                    dcc.Tab(label="Solve times", children=[]),
+                    dcc.Tab(label="Solve times", children=solve_times_tab_layout()),
                     dcc.Tab(label="Solve frequency", children=[]),
                     dcc.Tab(label="Progress", children=[]),
                 ]
@@ -70,15 +44,3 @@ def build_app(app):
 #        ],
 #    )
 #    return app
-
-# app.layout = html.Div(
-#    [
-#        dcc.Tabs(
-#            [
-#                dcc.Tab(label="Tab one", children=[]),
-#                dcc.Tab(label="Tab two", children=[dcc.Graph]),
-#                dcc.Tab(label="Tab three", children=[dcc.Graph]),
-#            ]
-#        )
-#    ]
-# )
