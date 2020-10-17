024c0f84b0ec9f098d77480950f74229b48d276e
Jurgen Hanekom <hanekom.jurgen@bcg.com>
2020-10-17 19:31:54 +0930

    abstracted layout

--- app.py
+++ app.py
@@ -1,53 +1,15 @@
-import pandas as pd
 import os
 from dotenv import load_dotenv
 
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output, State
-
-
-from cubeviz.charts import plot_solve_time_series
-from cubeviz.io import parse_upload_content
-from cubeviz.etl import process_timiks_data
 from cubeviz.server import app
+from cubeviz.layout import build_app
 import cubeviz.callback
 
 load_dotenv()
 
 
-def get_app(app):
-    app.layout = html.Div(
-        [
-            dcc.Upload(
-                id="upload-data",
-                children=html.Div(["Drag and Drop or ", html.A("Select Files")]),
-                style={
-                    "width": "100%",
-                    "height": "60px",
-                    "lineHeight": "60px",
-                    "borderWidth": "1px",
-                    "borderStyle": "dashed",
-                    "borderRadius": "5px",
-                    "textAlign": "center",
-                    "margin": "10px",
-                },
-                multiple=False,
-            ),
-            html.Div(id="div-solve-time-series"),
-            dcc.Dropdown(
-                id="dropdown",
-                options=[{"label": i, "value": i} for i in ["LA", "NYC", "MTL"]],
-                value="LA",
-            ),
-            html.Div(id="display-value"),
-        ],
-    )
-    return app
-
-
 if __name__ == "__main__":
-    app = get_app(app)
+    app = build_app(app)
 
     environment = os.getenv("ENVIRONMENT")
     if environment == "local":
---
+++ cubeviz/layout/base.py
@@ -0,0 +1,26 @@
+import dash_html_components as html
+import dash_core_components as dcc
+
+
+def build_app(app):
+    app.layout = html.Div(
+        [
+            dcc.Upload(
+                id="upload-data",
+                children=html.Div(["Drag and Drop or ", html.A("Select Files")]),
+                style={
+                    "width": "100%",
+                    "height": "60px",
+                    "lineHeight": "60px",
+                    "borderWidth": "1px",
+                    "borderStyle": "dashed",
+                    "borderRadius": "5px",
+                    "textAlign": "center",
+                    "margin": "10px",
+                },
+                multiple=False,
+            ),
+            html.Div(id="div-solve-time-series"),
+        ],
+    )
+    return app
