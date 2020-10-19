9788443cf55fe6de048bf8b65f5fa647be5e343f
Jurgen Hanekom <hanekom.jurgen@bcg.com>
2020-10-19 21:16:18 +0930

    Remove un needed function calls

--- cubeviz/etl/parsing.py
+++ cubeviz/etl/parsing.py
@@ -3,19 +3,16 @@ import io
 import pandas as pd
 import dash_html_components as html
 
-from cubeviz.config import cubeviz_config
-from cubeviz.models import CVEnhanced
+from cubeviz.models import CVBase
 from cubeviz.etl.process_data_sources import (
     get_data_source_format,
     parse_timiks_to_base,
     UploadFormats,
 )
-from cubeviz.etl.enhance import enhance_base_data
 
 
-def parse_upload_content(contents: str, filename: str, date) -> CVEnhanced:
+def parse_upload_content(contents: str, filename: str, date) -> CVBase:
     content_type, content_string = contents.split(",")
-    window_sizes = cubeviz_config.window_sizes
 
     decoded = base64.b64decode(content_string)
     try:
@@ -27,13 +24,10 @@ def parse_upload_content(contents: str, filename: str, date) -> CVEnhanced:
             df = pd.read_excel(io.BytesIO(decoded))
 
         data_format = get_data_source_format(df)
-
         if data_format == UploadFormats.TIMIKS:
             df = parse_timiks_to_base(df)
         else:
             raise Exception("Uh oh!")
-
-        df = enhance_base_data(df, window_sizes)
 
     except Exception as e:
         print(e)
