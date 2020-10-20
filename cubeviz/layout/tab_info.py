8f578e24b68a431a35a6cecddcc6cfff8e6abb92
Jurgen Hanekom <hanekom.jurgen@bcg.com>
2020-10-20 20:37:29 +0930

    Added file name display

--- cubeviz/layout/tab_info.py
+++ cubeviz/layout/tab_info.py
@@ -1,6 +1,7 @@
 import dash_core_components as dcc
 import dash_html_components as html
 from cubeviz.helpers import load_text
+from cubeviz.themeing import CVColors
 
 
 def upload_window():
@@ -15,5 +16,22 @@ def info_markdown():
     return dcc.Markdown(load_text("cubeviz/markdown/info.md"))
 
 
+def uploaded_file_name():
+    return html.H3(
+        id="h2-uploaded-file-name",
+    )
+
+
+def get_title_value(file_name):
+    return [
+        "Uploaded file: ",
+        html.Span(
+            id="span-file-name",
+            children=file_name,
+            style={"color": CVColors.PRIMARY_BLUE},
+        ),
+    ]
+
+
 def info_tab_layout():
-    return [upload_window(), info_markdown()]
+    return [upload_window(), uploaded_file_name(), info_markdown()]
